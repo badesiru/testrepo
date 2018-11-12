c7ba21f01a53b372a6007398d7d228c5ce435465
badesiru <34199526+badesiru@users.noreply.github.com>
2018-11-12 16:50:29 -0500

    Add files via upload

---
+++ 2DArrayCodeChallenge.rb
@@ -0,0 +1,31 @@
+multi = Array.new(3) { Array.new }
+multi [0][0] = 4
+multi [0][1] = 5
+multi [0][2] = 10
+multi [0][3] = 11
+multi [0][4] = 13
+multi [0][5] = 17
+
+
+
+multi [1][0] = -1
+multi [1][1] = -8
+multi [1][2] = 5
+multi [1][3] = -1
+multi [1][4] = -8
+multi [1][5] = 5
+
+
+
+
+multi [2][0] = 7
+multi [2][1] = -6
+multi [2][2] = -3
+multi [2][3] = 18
+multi [2][4] = -1
+multi [2][5] = -8
+
+
+
+
+puts  multi + '\n'
